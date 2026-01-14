157ec05cfeeb6c13f00ac1e04520dd40934fc9f7
yukkuri <reityan.sakura0104@gmail.com>
2026-01-14 17:19:29 +0900

    pTag の定義を関数外に移動し、コードの重複を削除

--- keyword/script.js
+++ keyword/script.js
@@ -1,5 +1,17 @@
+const pTag = document.querySelector("p")
 function input() {
     const input = document.querySelector("#inner")
-    const pTag = document.querySelector("p")
     pTag.textContent = input.value
+}
+
+function search() {
+    const searchTxt = document.querySelector("#txt").value
+    let temp = pTag.textContent;
+    let newSpan = temp.replaceAll(searchTxt,`<span>${searchTxt}</span>`)
+    pTag.innerHTML = newSpan;
+    const spans = document.querySelectorAll("span")
+    spans.forEach(element => {
+        color = Math.random().toString(16).slice(-6)
+        element.style.backgroundColor = `#${color}`
+    });
 }
